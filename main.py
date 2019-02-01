da1676ef20c8820deb38aeee58e7375033b78566
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-01 17:44:32 -0500

    FIXME: Add arbitrary game loop

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import cv2
 
-print("Hello, World")
+# FIXME: arbitrary video loop
+class App:
+    """Represents main application class"""
+
+    def __init__(self):
+        """Initialize cameras"""
+
+        self._cameras = []
+
+        for i in range(4):
+            self._cameras.append(cv2.VideoCapture(i))
+
+    def run(self):
+        """Run the application code"""
+
+        while True:
+            pass
+
+
+if __name__ == "__main__":
+    App().run()
