7abae2e0919df80728f3b19b287e125a21cc31fa
Sagar Patil <sagarreddypatil@gmail.com>
2019-02-02 11:53:47 -0500

    bobs or vegana

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import cv2
 
-# FIXME: arbitrary video loop
-class App:
-    """Represents main application class"""
 
-    def __init__(self):
-        """Initialize cameras"""
-
-        self._cameras = []
-
-        for i in range(4):
-            self._cameras.append(cv2.VideoCapture(i))
-
-    def run(self):
-        """Run the application code"""
-
-        while True:
-            pass
+def main():
+    pass
 
 
 if __name__ == "__main__":
-    App().run()
+    main()
