e0cbf9627b62c13dff90956af894effc50d57221
Sagar Patil <sagarreddypatil@gmail.com>
2019-02-15 17:43:17 -0500

    New Grip Stuff. TODO: Put the new code into main script

--- GRIP_Files/new_vision_tuned.py
+++ GRIP_Files/new_vision_tuned.py
@@ -12,26 +12,20 @@ class VisionPipeline:
         """initializes all values to presets or None if need to be set
         """
 
+        self.__hsv_threshold_hue = [77.86673153833091, 125.3912518202375]
+        self.__hsv_threshold_saturation = [137.8511306766397, 209.3978932665679]
+        self.__hsv_threshold_value = [59.60244327371679, 255.0]
+
+        self.hsv_threshold_output = None
+
+        self.__resize_image_input = self.hsv_threshold_output
         self.__resize_image_width = 320.0
-        self.__resize_image_height = 180.0
+        self.__resize_image_height = 240.0
         self.__resize_image_interpolation = cv2.INTER_CUBIC
 
         self.resize_image_output = None
 
-        self.__blur_input = self.resize_image_output
-        self.__blur_type = BlurType.Gaussian_Blur
-        self.__blur_radius = 4.716981132075471
-
-        self.blur_output = None
-
-        self.__rgb_threshold_input = self.blur_output
-        self.__rgb_threshold_red = [205, 255.0]
-        self.__rgb_threshold_green = [205, 255.0]
-        self.__rgb_threshold_blue = [205, 255.0]
-
-        self.rgb_threshold_output = None
-
-        self.__find_contours_input = self.rgb_threshold_output
+        self.__find_contours_input = self.resize_image_output
         self.__find_contours_external_only = False
 
         self.find_contours_output = None
@@ -41,7 +35,7 @@ class VisionPipeline:
         self.convex_hulls_output = None
 
         self.__filter_contours_contours = self.convex_hulls_output
-        self.__filter_contours_min_area = 100.0
+        self.__filter_contours_min_area = 50.0
         self.__filter_contours_min_perimeter = 0.0
         self.__filter_contours_min_width = 0.0
         self.__filter_contours_max_width = 1000.0
@@ -60,20 +54,16 @@ class VisionPipeline:
         """
         Runs the pipeline and sets all outputs to new values.
         """
+        # Step HSV_Threshold0:
+        self.__hsv_threshold_input = source0
+        (self.hsv_threshold_output) = self.__hsv_threshold(self.__hsv_threshold_input, self.__hsv_threshold_hue, self.__hsv_threshold_saturation, self.__hsv_threshold_value)
+
         # Step Resize_Image0:
-        self.__resize_image_input = source0
+        self.__resize_image_input = self.hsv_threshold_output
         (self.resize_image_output) = self.__resize_image(self.__resize_image_input, self.__resize_image_width, self.__resize_image_height, self.__resize_image_interpolation)
 
-        # Step Blur0:
-        self.__blur_input = self.resize_image_output
-        (self.blur_output) = self.__blur(self.__blur_input, self.__blur_type, self.__blur_radius)
-
-        # Step RGB_Threshold0:
-        self.__rgb_threshold_input = self.blur_output
-        (self.rgb_threshold_output) = self.__rgb_threshold(self.__rgb_threshold_input, self.__rgb_threshold_red, self.__rgb_threshold_green, self.__rgb_threshold_blue)
-
         # Step Find_Contours0:
-        self.__find_contours_input = self.rgb_threshold_output
+        self.__find_contours_input = self.resize_image_output
         (self.find_contours_output) = self.__find_contours(self.__find_contours_input, self.__find_contours_external_only)
 
         # Step Convex_Hulls0:
@@ -84,6 +74,20 @@ class VisionPipeline:
         self.__filter_contours_contours = self.convex_hulls_output
         (self.filter_contours_output) = self.__filter_contours(self.__filter_contours_contours, self.__filter_contours_min_area, self.__filter_contours_min_perimeter, self.__filter_contours_min_width, self.__filter_contours_max_width, self.__filter_contours_min_height, self.__filter_contours_max_height, self.__filter_contours_solidity, self.__filter_contours_max_vertices, self.__filter_contours_min_vertices, self.__filter_contours_min_ratio, self.__filter_contours_max_ratio)
 
+
+    @staticmethod
+    def __hsv_threshold(input, hue, sat, val):
+        """Segment an image based on hue, saturation, and value ranges.
+        Args:
+            input: A BGR numpy.ndarray.
+            hue: A list of two numbers the are the min and max hue.
+            sat: A list of two numbers the are the min and max saturation.
+            lum: A list of two numbers the are the min and max value.
+        Returns:
+            A black and white numpy.ndarray.
+        """
+        out = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
+        return cv2.inRange(out, (hue[0], sat[0], val[0]),  (hue[1], sat[1], val[1]))
 
     @staticmethod
     def __resize_image(input, width, height, interpolation):
@@ -97,42 +101,6 @@ class VisionPipeline:
             A numpy.ndarray of the new size.
         """
         return cv2.resize(input, ((int)(width), (int)(height)), 0, 0, interpolation)
-
-    @staticmethod
-    def __blur(src, type, radius):
-        """Softens an image using one of several filters.
-        Args:
-            src: The source mat (numpy.ndarray).
-            type: The blurType to perform represented as an int.
-            radius: The radius for the blur as a float.
-        Returns:
-            A numpy.ndarray that has been blurred.
-        """
-        if(type is BlurType.Box_Blur):
-            ksize = int(2 * round(radius) + 1)
-            return cv2.blur(src, (ksize, ksize))
-        elif(type is BlurType.Gaussian_Blur):
-            ksize = int(6 * round(radius) + 1)
-            return cv2.GaussianBlur(src, (ksize, ksize), round(radius))
-        elif(type is BlurType.Median_Filter):
-            ksize = int(2 * round(radius) + 1)
-            return cv2.medianBlur(src, ksize)
-        else:
-            return cv2.bilateralFilter(src, -1, round(radius), round(radius))
-
-    @staticmethod
-    def __rgb_threshold(input, red, green, blue):
-        """Segment an image based on color ranges.
-        Args:
-            input: A BGR numpy.ndarray.
-            red: A list of two numbers the are the min and max red.
-            green: A list of two numbers the are the min and max green.
-            blue: A list of two numbers the are the min and max blue.
-        Returns:
-            A black and white numpy.ndarray.
-        """
-        out = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
-        return cv2.inRange(out, (red[0], green[0], blue[0]),  (red[1], green[1], blue[1]))
 
     @staticmethod
     def __find_contours(input, external_only):
@@ -210,5 +178,4 @@ class VisionPipeline:
         return output
 
 
-BlurType = Enum('BlurType', 'Box_Blur Gaussian_Blur Median_Filter Bilateral_Filter')
 
