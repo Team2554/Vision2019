02ad0655adf33019053ebefebfb810dd6a595d8c
Sagar Patil <sagarreddypatil@gmail.com>
2019-03-09 13:29:14 -0500

    Finally, some good working code

--- local_testing_new.py
+++ local_testing_new.py
@@ -43,56 +43,55 @@ def angleToTarget(img, contours):
                 finalCnts.append(contours[idx])
                 baseAngle = i
             else:
-                diff = abs(abs(i) - abs(baseAngle)))
+                diff = abs(abs(i) - abs(baseAngle))
                 if (diff - 80) < 20:
                     finalCnts.append(contours[idx])
                     break
-        if len(finalCnts) < 2:
-            return new_image, angle
-        
-        finalCnts = list(sorted(finalCnts, key=cv2.contourArea))[::-1]
-        cnt1 = finalCnts[0]
-        cnt2 = finalCnts[1]
+        if not len(finalCnts) < 2:
+            finalCnts = list(sorted(finalCnts, key=cv2.contourArea))[::-1]
+            cnt1 = finalCnts[0]
+            cnt2 = finalCnts[1]
 
-        M1 = cv2.moments(cnt1)
-        M2 = cv2.moments(cnt2)
+            M1 = cv2.moments(cnt1)
+            M2 = cv2.moments(cnt2)
 
-        center1 = (int(M1["m10"] / M1["m00"]), int(M1["m01"] / M1["m00"]))
-        center2 = (int(M2["m10"] / M2["m00"]), int(M2["m01"] / M2["m00"]))
+            center1 = (int(M1["m10"] / M1["m00"]), int(M1["m01"] / M1["m00"]))
+            center2 = (int(M2["m10"] / M2["m00"]), int(M2["m01"] / M2["m00"]))
 
-        cv2.circle(
-            img=new_image, center=center1, radius=3, color=(0, 0, 255), thickness=-1
-        )
-        cv2.circle(
-            img=new_image, center=center2, radius=3, color=(0, 0, 255), thickness=-1
-        )
+            # cv2.circle(
+            #     img=new_image, center=center1, radius=3, color=(0, 0, 255), thickness=-1
+            # )
+            # cv2.circle(
+            #     img=new_image, center=center2, radius=3, color=(0, 0, 255), thickness=-1
+            # )
 
-        # Draw the midpoint of both of these contours
-        targetCenter = (
-            int((center1[0] + center2[0]) / 2),
-            int((center1[1] + center2[1]) / 2),
-        )
-        cv2.circle(
-            img=new_image,
-            center=targetCenter,
-            radius=3,
-            color=(0, 0, 255),
-            thickness=-1,
-        )
+            # Draw the midpoint of both of these contours
+            targetCenter = (
+                int((center1[0] + center2[0]) / 2),
+                int((center1[1] + center2[1]) / 2),
+            )
+            cv2.circle(
+                img=new_image,
+                center=targetCenter,
+                radius=3,
+                color=(0, 0, 255),
+                thickness=-1,
+            )
 
-        angle = (targetCenter[0] - CENTER_WIDTH_PIXEL) * DEG_PER_PIXEL
-        cv2.putText(
-            new_image,
-            str(round(angle, 2)) + " deg",
-            (0, 25),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            color=(0, 255, 255),
-            thickness=2,
-        )
+            angle = (targetCenter[0] - CENTER_WIDTH_PIXEL) * DEG_PER_PIXEL
+            cv2.putText(
+                new_image,
+                str(round(angle, 2)) + " deg",
+                (0, 25),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                color=(0, 255, 255),
+                thickness=2,
+            )
 
-        cv2.line(new_image, targetCenter, imgCenter, (255, 0, 0), 2)
+            cv2.line(new_image, targetCenter, imgCenter, (255, 0, 0), 2)
     return new_image, angle
+
 
 def main():
     cap = cv2.VideoCapture("http://frcvision.local:1181/stream.mjpg")
@@ -104,9 +103,13 @@ def main():
 
             pipeline.process(img)
             img = cv2.resize(img, (320, 240), 0, 0, cv2.INTER_CUBIC)
-            img, angle = angleToTarget(img, pipeline.convex_hulls_output)
+            img, angle = angleToTarget(img, pipeline.filter_contours_output)
             cv2.imshow("image", img)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         except Exception as e:
             print("A big gae occurred: ", e)
+
+
+if __name__ == "__main__":
+    main()
